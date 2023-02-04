0e06b9ed267205312ac5fcc4af5a9707ad8171c5
Jimi <contact@jimi.sexy>
2023-02-04 18:36:33 +0100

    ✏️  Added ability for users to change profile information

--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -28,3 +28,25 @@ document.querySelector('#shuffle-image').addEventListener('click', () => {
     let newAvatarUrl = `https://source.boringavatars.com/${(document.querySelector('#avatarType') as HTMLSelectElement).value}/400/${randomNum}`;
     (document.querySelector('#profile-image img') as HTMLImageElement).src = newAvatarUrl;
 });
+
+document.querySelector('#save-button').addEventListener('click', () => {
+    fetch(`${apiBase}/user`, {
+        method: 'PATCH',
+        headers: {
+            "Authorization": localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            name: (document.querySelector('#usernameInput') as HTMLInputElement).value,
+            avatarUrl: (document.querySelector('#profile-image img') as HTMLImageElement).src
+        })
+    }).then(async res => {
+        const data = await res.json();
+        if (res.status === 200) {
+            (document.querySelector('#profile-image img') as HTMLImageElement).src = data.avatarUrl;
+            (document.querySelector('#usernameInput') as HTMLInputElement).value = data.name;
+            (document.querySelector('#emailInput') as HTMLInputElement).value = data.email;
+        } else {
+            alert(data.error);
+        }
+    });
+});
