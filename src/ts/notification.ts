d44efc8988f27766850c6b3e58a18ba9f2e01dd5
Jimi <jimiherken@icloud.com>
2023-02-12 20:47:55 +0100

    🚨added Custom alert

--- src/ts/notification.ts
+++ src/ts/notification.ts
@@ -27,3 +27,27 @@ class CustomNotification {
         this.element.remove();
     }
 }
+
+function customAlert(text: string, title?:string, icon?:string) {
+    const alert = document.createElement("div");
+    alert.id = "alert";
+
+    const alertIcon = document.createElement('i');
+    alertIcon.setAttribute("data-feather", icon || "alert-circle");
+
+    const alertTitle = document.createElement('h1');
+    alertTitle.innerText = title || "Alert";
+
+    const alertText = document.createElement('span');
+    alertText.innerText = text;
+
+    const alertButton = document.createElement('button');
+    alertButton.id = "alert-ok";
+    alertButton.innerText = "OK";
+    alertButton.addEventListener('click', () => document.querySelector('#alert').remove());
+
+    alert.append(alertIcon, alertTitle, alertText, alertButton);
+    document.body.appendChild(alert);
+    // @ts-ignore
+    feather.replace();
+}
